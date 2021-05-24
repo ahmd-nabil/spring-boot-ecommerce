96180a054835cef479cffc129327b9606e0e2e5b
Ahmed Nabil <ahmednabil29999@gmail.com>
2021-05-25 00:51:32 +0200

    add ngbootstrap library

--- ui/angular-ecommerce/src/app/app.module.ts
+++ ui/angular-ecommerce/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 const routes: Routes = [
   {path: 'category/:id', component: ProductListComponent},
@@ -30,7 +31,8 @@ const routes: Routes = [
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
